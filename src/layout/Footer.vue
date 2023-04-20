491bb1823cc3f721ce537ac407439a17808fa838
615Alberto615 <alberto.encinas@ucb.edu.bo>
2023-04-19 21:20:54 -0400

    Mensaje de confirmación

---
+++ src/layout/Footer.vue
@@ -0,0 +1,23 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <footer class="mt-10">
+        <div class="container-fluid mx-auto">
+            <center>
+                <BaseCard class=""> 
+                <div class="flex justify-between items-center">
+                   
+                        <div>
+                            
+                                <p>All rights reserved ©UCB 2023</p>
+                            
+                        </div>
+                    
+                </div>
+                </BaseCard>
+            </center>
+        </div>
+    </footer>
+</template>
