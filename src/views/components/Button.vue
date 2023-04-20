b592a618bdc3224760124328b897a92523634a5a
cristhian_aguilar_m <cristhian.aguilar@ucb.edu.bo>
2023-04-19 23:46:49 -0400

    Agregando Cronograma

--- src/views/components/Button.vue
+++ src/views/components/Button.vue
@@ -1,105 +1,106 @@
 <script setup>
 
+import { 
+        dashboardOne, 
+        dashboardTwo, 
+        splineAreaWidgetTwo, 
+        splineAreaWidgetThree
+  } from '@/data/dashboard.v1.js';
+
 const colors = [
   {
     colorName: 'bg-primary text-white',
-    theme: 'Primary'
+    theme: 'Evento - Dia Lunes 20 de Septiembre'
   },
   {
     colorName: 'bg-success text-white',
-    theme: 'Success'
+    theme: 'Evento - Dia Martes 21 de Septiembre'
   },
   {
     colorName: 'bg-danger text-white',
-    theme: 'Danger'
+    theme: 'Evento - Dia Miercoles 23 de Septiembre'
   },
   {
     colorName: 'bg-warning text-white',
-    theme: 'Warning'
+    theme: 'Evento - Dia Jueves 24 de Septiembre'
   },
   {
     colorName: 'bg-info text-white',
-    theme: 'Info'
+    theme: 'Evento - Dia Viernes 25 de Septiembre'
   },
   {
     colorName: 'bg-light text-white',
-    theme: 'Light'
+    theme: 'Evento - Dia Sabado 26 de Septiembre'
   },
   {
     colorName: 'bg-dark text-white',
-    theme: 'Dark'
+    theme: 'Evento - Dia Domingo 27 de Septiembre'
   },
 ]
 const colorsOutlined = [
   {
     colorName: 'border border-primary text-primary hover:bg-primary hover:text-white  ',
-    theme: 'Primary'
+    theme: 'Evento - Dia Lunes 20 de Septiembre'
   },
   {
     colorName: 'border border-success text-success hover:bg-success hover:text-white',
-    theme: 'Success'
+    theme: 'Evento - Dia Martes 21 de Septiembre'
   },
   {
     colorName: 'border border-danger text-danger hover:bg-danger hover:text-white',
-    theme: 'Danger'
+    theme: 'Evento - Dia Miercoles 23 de Septiembre'
   },
   {
     colorName: 'border border-warning text-warning hover:bg-warning hover:text-white',
-    theme: 'Warning'
+    theme: 'Evento - Dia Jueves 24 de Septiembre'
   },
   {
     colorName: 'border border-info text-info hover:bg-info hover:text-white',
-    theme: 'Info'
+    theme: 'Evento - Dia Viernes 25 de Septiembre'
   },
   {
     colorName: 'border border-light text-light hover:bg-light hover:text-white',
-    theme: 'Light'
+    theme: 'Evento - Dia Sabado 26 de Septiembre'
   },
   {
     colorName: 'border border-dark text-dark hover:bg-dark hover:text-white',
-    theme: 'Dark'
+    theme: 'Evento - Dia Domingo 27 de Septiembre'
   },
 ]
 </script>
 
 <template>
   <div class="container mx-auto">
+        <Breadcrumbs parentTitle="Dashboard" subParentTitle="Dashboard v1" />
+        <div class="container-fluid mx-auto">
+            <center>
+                <BaseCard class="">
+                    <div class="flex justify-between items-center">
+                        <div>
+                            <p class="text-2xl font-bold">CRONOGRAMA -  FERIA DEL EMPLEO</p><br>
+                            <p>
+                                En este pagina se publicaran los eventos que se realizaran en la feria del empleo, y se iran subiendo
+                                las fechas programadas para cada evento mediante un link que se proporcionara y se mostrara en la pagina.
+                            </p>
+                            
+
+                        </div>
+                        
+                    </div>
+                </BaseCard>
+            </center>
+        </div>
+    </div>
+
+  <div class="container mx-auto">
      <Breadcrumb parentTitle='Button' subParentTitle='Buttons' />
 
 
     <div class="mt-10">
-      <div class="grid md:grid-cols-2 sm:grid-cols-1 gap-4">
+      <div class="grid md:grid-cols-1 sm:grid-cols-1 gap-4">
         <div>
           <BaseCard>
-            <div class="card-title mb-5">Default Buttons</div>
-            <BaseBtn 
-              v-for="(color, index) in colors"
-              :key="index"
-              :class="color.colorName" 
-              class="mb-3 mr-3"
-              
-            >
-              {{color.theme}}
-            </BaseBtn>
-          </BaseCard>
-        </div>
-        <div>
-          <BaseCard>
-            <div class="card-title mb-5">Outline Buttons</div>
-            <BaseBtn 
-              v-for="(color, index) in colorsOutlined"
-              :class="color.colorName"
-              :key="index"
-              
-              class="mb-3 mr-3"
-            >
-              {{color.theme}}
-            </BaseBtn>
-          </BaseCard>
-        </div>
-        <div>
-          <BaseCard>
-            <div class="card-title mb-5">Button Block</div>
+            <div class="card-title mb-12">REUNIONES PROGRAMADAS</div>
             <BaseBtn 
               v-for="(color, index) in colors"
               :key="index"
@@ -111,64 +112,7 @@ const colorsOutlined = [
             </BaseBtn>
           </BaseCard>
         </div>
-        <div>
-          <BaseCard>
-            <div class="card-title mb-5">Large & Small Buttons</div>
-            <div>
-              <BaseBtn 
-                v-for="(color, index) in colors"
-                :key="index"
-                sm
-                :class="color.colorName" 
-                class="mb-3 mr-3"
-              >
-                {{color.theme}}
-              </BaseBtn>
-            </div>
-            <div>
-              <BaseBtn 
-                v-for="(color, index) in colors"
-                :key="index"
-                :class="color.colorName" 
-                class="mb-3 mr-3"
-              >
-                {{color.theme}}
-              </BaseBtn>
-            </div>
-            <div>
-              <BaseBtn 
-                v-for="(color, index) in colors"
-                :key="index"
-                :class="color.colorName" 
-                xl
-                class="mb-3 mr-3"
-              >
-                {{color.theme}}
-              </BaseBtn>
-            </div>
-          </BaseCard>
-        </div>
-        <div>
-          <BaseCard>
-            <div class="card-title mb-5">Buttons with Icon</div>
-            
-            <div>
-              <BaseBtn 
-                
-                class="mb-3 mr-3 bg-primary text-white"
-              >
-                <i class="i-Gear-2"></i>
-                
-              </BaseBtn>
-              <BaseBtn 
-                class="mb-3 mr-3 bg-success text-white"
-              >
-                <i class="i-Gear-2 mr-1"></i>
-                Hello World 
-              </BaseBtn>
-            </div>
-          </BaseCard>
-        </div>
+        
         
         
       </div>
