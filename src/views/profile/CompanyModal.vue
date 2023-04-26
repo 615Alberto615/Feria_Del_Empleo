b86493f0cfcb2f862b177c51e86bdea78852f6d8
615Alberto615 <alberto.encinas@ucb.edu.bo>
2023-04-25 21:06:47 -0400

    empresas terminado

---
+++ src/views/profile/CompanyModal.vue
@@ -0,0 +1,126 @@
+<template>
+    <transition name="modal">
+        <div v-if="isOpen" class="modal fixed inset-0 z-50 flex items-center justify-center">
+      <div class="modal-overlay absolute inset-0 bg-black opacity-50" @click="emitClose"></div>
+      <div class="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50">
+        <div class="modal-content p-6 overflow-y-auto">
+            <button class="close-modal absolute top-0 right-0 cursor-pointer p-4" @click="emitClose" style="background-color: transparent; border: none; pointer-events: none;">
+                <img src="/close-circle-outline.svg" alt="Close" class="w-5 h-5" style="pointer-events: auto;" />
+            </button>
+            <h3 class="text-2xl">{{ company.name }}</h3>
+            <p>{{ company.category }}</p>
+            <p>{{ company.description }}</p>
+                  <!-- Agregar video -->
+                  <div class="flex mt-6">
+            <div class="w-1/2 mr-2">
+                <BaseCard>
+                    <div class="video-container">
+                      <iframe :src="company.videoUrl" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </BaseCard>
+            </div>
+            <div class="w-1/2 ml-2">
+              <BaseCard>
+                <p class="text-lg font-semibold">¿De qué trata la empresa?</p>
+                <p>{{ company.summary }}</p>
+              </BaseCard>
+            </div>
+          </div>
+
+      <!-- Agregar tabla de oferta laboral -->
+      <p class="text-lg font-medium mt-6">OFERTA LABORAL</p>
+      <table class="table-auto mt-2">
+        <thead>
+          <tr>
+            <th class="py-3 px-4 border-b border-gray-400 bg-gray-100 font-semibold uppercase text-sm text-gray-600 text-left">Nombre del puesto</th>
+                  <th class="py-3 px-4 border-b border-gray-400 bg-gray-100 font-semibold uppercase text-sm text-gray-600 text-left">Descripción de requisitos</th>
+                  <th class="py-3 px-4 border-b border-gray-400 bg-gray-100 font-semibold uppercase text-sm text-gray-600 text-left">Descripción de funciones</th>
+           </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(job, index) in company.jobOffers" :key="index">
+            <td class="border px-4 py-2">{{ job.name }}</td>
+            <td class="border px-4 py-2">{{ job.requirements }}</td>
+            <td class="border px-4 py-2">{{ job.functions }}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <!-- Agregar información de contacto -->
+      <div class="mt-6">
+        <BaseCard>
+            <p class="text-lg font-medium">Contacto</p>
+            <div class="mt-2">
+            <p class="text-sm font-medium">Nombre: {{ company.contactName }}</p>
+            <p class="text-sm font-medium">Correo electrónico: {{ company.contactEmail }}</p>
+            <p class="text-sm font-medium">Celular: {{ company.contactPhone }}</p>
+            </div>
+        </BaseCard>
+      </div>
+
+    </div>
+  </div>
+</div>
+</transition>
+</template>
+  
+  <script>
+  export default {
+    props: {
+      isOpen: {
+        type: Boolean,
+        required: true,
+      },
+      company: {
+        type: Object,
+        required: true,
+      },
+    },
+    methods: {
+      emitClose() {
+        this.$emit('close');
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .modal {
+    transition: opacity 0.25s ease;
+  }
+  
+  .modal-enter, .modal-leave-active {
+    opacity: 0;
+  }
+  
+  .modal-content {
+    position: relative;
+    max-height: calc(100vh - 200px); /* Altura máxima de la modal */
+  }
+
+  .close-modal {
+    top: 0.5rem;
+    right: 0.5rem;
+  }
+
+  .video-container {
+    position: relative;
+    padding-bottom: 56.25%; /* Establece un aspect ratio de 16:9 */
+    height: 0;
+  }
+
+  .video-container iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+  }
+  .modal-container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 40%;
+}   
+</style>
--- src/views/profile/ProfileTwo.vue
+++ src/views/profile/ProfileTwo.vue
@@ -1,217 +1,199 @@
 <script setup>
-    import { 
-        dashboardOne, 
-        dashboardTwo, 
-        splineAreaWidgetTwo, 
-        splineAreaWidgetThree
-    } from '@/data/dashboard.v1.js';
-   
+import { ref, computed } from 'vue';
+import CompanyModal from './CompanyModal.vue';
+const companies = [
+    {
+        id: 1,
+        name: 'Empresa 1',
+        category: 'Banco',
+        logo: '/bnb.png',
+        description: 'Descripcion general de lo que es la empresa...',
+        socialMedia: {
+            linkedin: 'https://bo.linkedin.com/',
+            facebook: 'https://es-la.facebook.com/',
+            twitter: 'https://twitter.com/',
+            instagram: 'https://www.instagram.com/',
+        },
+        videoUrl: 'https://www.youtube.com/embed/_i0w6AOO4mY',
+        summary: 'Lorem ipsum dolor sit',
+        contactName: 'Nombre de contacto',
+        contactEmail: 'email@email.com',
+        contactPhone: '12345678',
+        jobOffers: [
+            {
+                name: 'Puesto 1',
+                requirements: 'L',
+                functions: 'L',
+            },
+            {
+                name: 'Puesto 2',
+                requirements: 'L',
+                functions: 'L',
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: 'Empresa 2',
+        category: 'Empresa de ...',
+        logo: '/bcp.png',
+        description: 'Descripcion general de lo que es la empresa...',
+        socialMedia: {
+            linkedin: 'https://bo.linkedin.com/',
+            facebook: 'https://es-la.facebook.com/',
+            twitter: 'https://twitter.com/',
+            instagram: 'https://www.instagram.com/',
+        },
+
+        
+    },
+    {
+        id: 3,
+        name: 'Empresa 3',
+        category: 'Empresa de ...',
+        logo: '/mercan.jpg',
+        description: 'Descripcion general de lo que es la empresa...',
+        socialMedia: {
+            linkedin: 'https://bo.linkedin.com/',
+            facebook: 'https://es-la.facebook.com/',
+            twitter: 'https://twitter.com/',
+            instagram: 'https://www.instagram.com/',
+        },
+        
+    },
+    {
+        id: 4,
+        name: 'Empresa 4',
+        category: 'Empresa de ...',
+        logo: '/union.png',
+        description: 'Descripcion general de lo que es la empresa...',
+        socialMedia: {
+            linkedin: 'https://bo.linkedin.com/',
+            facebook: 'https://es-la.facebook.com/',
+            twitter: 'https://twitter.com/',
+            instagram: 'https://www.instagram.com/',
+        },
+        
+    },
     
+    
+    // ... más objetos de empresas
+];
+const searchText = ref('');
+
+const filteredCompanies = computed(() => {
+    return companies.filter((company) => {
+        const searchValue = searchText.value.toLowerCase();
+        return (
+            company.name.toLowerCase().includes(searchValue) ||
+            company.category.toLowerCase().includes(searchValue)
+        );
+    });
+});
+
+const showModal = ref(false);
+const selectedCompany = ref(null);
+
+const showCompanyModal = (company) => {
+  selectedCompany.value = company;
+  showModal.value = true;
+};
+
+const closeCompanyModal = () => {
+  showModal.value = false;
+};
 </script>
 
 <template>
     <div class="container mx-auto">
-        <Breadcrumbs parentTitle="Dashboard" subParentTitle="Dashboard v1" />
-        <div class="grid grid-cols-12 gap-5">
-            
-            <div
-                class="
-                    col-span-12
-                    xl:col-span-3
-                    lg:col-span-3
-                    md:col-span-6
-                    sm:col-span-6
-                "
+      <Breadcrumbs parentTitle="Dashboard" subParentTitle="Dashboard v1" />
+      <div class="mb-5">
+        <input
+          type="text"
+          v-model="searchText"
+          placeholder="Buscar empresa..."
+          class="border border-gray-300 rounded p-2 w-full"
+        />
+      </div>
+      <div class="grid grid-cols-12 gap-5">
+        <div
+          v-for="company in filteredCompanies"
+          :key="company.id"
+          class="
+            col-span-12
+            xl:col-span-3
+            lg:col-span-3
+            md:col-span-6
+            sm:col-span-6
+          "
+        >
+          <BaseCard class="text-center">
+            <img
+              class="
+                w-20
+                h-20
+                m-auto
+                shadow-lg
+                avatar-md
+                rounded-full
+              "
+              :src="company.logo"
+              :alt="company.name"
+            />
+            <p class="text-base mt-4">{{ company.name }}</p>
+            <p class="text-xs text-gray-400">{{ company.category }}</p>
+            <p class="my-2 text-sm text-gray-500 mb-3">
+              {{ company.description }}
+            </p>
+            <BaseBtn
+              sm
+              class="bg-primary text-white rounded-full"
+              @click="showCompanyModal(company)"
             >
-                <BaseCard class="text-center">
-                    <img
-                        class="
-                            w-20
-                            h-20
-                            m-auto
-                            shadow-lg
-                            avatar-md
-                            rounded-full
-                        "
-                        src="/bnb.png"
-                        alt=""
-                    />
-                    <p class="text-base mt-4">Empresa 1</p>
-                    <p class="text-xs text-gray-400">Empresa de ...</p>
-                    <p class="my-2 text-sm text-gray-500 mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Recusandae cumque.
-                    </p>
-                    <BaseBtn sm class="bg-primary text-white rounded-full">
-                        Contacto Empresa
-                    </BaseBtn>
-                    <div class="mt-4">
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Linkedin-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Facebook-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Twitter text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Instagram text-xs"></i>
-                        </router-link>
-
-                    </div>
-                </BaseCard>
-                
+              Más información
+            </BaseBtn>
+            <div class="mt-4">
+              <a
+                :href="company.socialMedia.linkedin"
+                target="_blank"
+                class="mr-2 hover:text-primary"
+                rel="noopener noreferrer"
+              >
+                <i class="i-Linkedin-2 text-xs"></i>
+              </a>
+              <a
+                :href="company.socialMedia.facebook"
+                target="_blank"
+                class="mr-2 hover:text-primary"
+                rel="noopener noreferrer"
+              >
+                <i class="i-Facebook-2 text-xs"></i>
+              </a>
+              <a
+                :href="company.socialMedia.twitter"
+                target="_blank"
+                class="mr-2 hover:text-primary"
+                rel="noopener noreferrer"
+              >
+                <i class="i-Twitter text-xs"></i>
+              </a>
+              <a
+                :href="company.socialMedia.instagram"
+                target="_blank"
+                class="mr-2 hover:text-primary"
+                rel="noopener noreferrer"
+              >
+                <i class="i-Instagram text-xs"></i>
+              </a>
             </div>
-            <!-- end of col -->
-            <div
-                class="
-                    col-span-12
-                    xl:col-span-3
-                    lg:col-span-3
-                    md:col-span-6
-                    sm:col-span-6
-                "
-            >
-                <BaseCard class="text-center">
-                    <img
-                        class="
-                            w-20
-                            h-20
-                            m-auto
-                            shadow-lg
-                            avatar-md
-                            rounded-full
-                        "
-                        src="/bcp.png"
-                        alt=""
-                    />
-                    <p class="text-base mt-4">Empresa 2</p>
-                    <p class="text-xs text-gray-400">Empresa de ...</p>
-                    <p class="my-2 text-sm text-gray-500 mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Recusandae cumque.
-                    </p>
-                    <BaseBtn sm class="bg-primary text-white rounded-full">
-                        Contacto Empresa
-                    </BaseBtn>
-                    <div class="mt-4">
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Linkedin-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Facebook-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Twitter text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Instagram text-xs"></i>
-                        </router-link>
-
-                    </div>
-                </BaseCard>
-                
-            </div>
-            <div
-                class="
-                    col-span-12
-                    xl:col-span-3
-                    lg:col-span-3
-                    md:col-span-6
-                    sm:col-span-6
-                "
-            >
-                <BaseCard class="text-center">
-                    <img
-                        class="
-                            w-20
-                            h-20
-                            m-auto
-                            shadow-lg
-                            avatar-md
-                            rounded-full
-                        "
-                        src="/mercan.jpg"
-                        alt=""
-                    />
-                    <p class="text-base mt-4">Empresa 3</p>
-                    <p class="text-xs text-gray-400">Empresa de ...</p>
-                    <p class="my-2 text-sm text-gray-500 mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Recusandae cumque.
-                    </p>
-                    <BaseBtn sm class="bg-primary text-white rounded-full">
-                        Contacto Empresa
-                    </BaseBtn>
-                    <div class="mt-4">
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Linkedin-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Facebook-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Twitter text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Instagram text-xs"></i>
-                        </router-link>
-
-                    </div>
-                </BaseCard>
-                
-            </div>
-            <div
-                class="
-                    col-span-12
-                    xl:col-span-3
-                    lg:col-span-3
-                    md:col-span-6
-                    sm:col-span-6
-                "
-            >
-                <BaseCard class="text-center">
-                    <img
-                        class="
-                            w-20
-                            h-20
-                            m-auto
-                            shadow-lg
-                            avatar-md
-                            rounded-full
-                        "
-                        src="/union.png"
-                        alt=""
-                    />
-                    <p class="text-base mt-4">Empresa 1</p>
-                    <p class="text-xs text-gray-400">Empresa de ...</p>
-                    <p class="my-2 text-sm text-gray-500 mb-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Recusandae cumque.
-                    </p>
-                    <BaseBtn sm class="bg-primary text-white rounded-full">
-                        Contacto Empresa
-                    </BaseBtn>
-                    <div class="mt-4">
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Linkedin-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Facebook-2 text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Twitter text-xs"></i>
-                        </router-link>
-                        <router-link to="" class="mr-2 hover:text-primary">
-                            <i class="i-Instagram text-xs"></i>
-                        </router-link>
-
-                    </div>
-                </BaseCard>
-                
-            </div>
-            
+          </BaseCard>
         </div>
+      </div>
+      <CompanyModal
+        :isOpen="showModal"
+        :company="selectedCompany"
+        @close="closeCompanyModal"
+      />
     </div>
-</template>
+  </template>
+  
